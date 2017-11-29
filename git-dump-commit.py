b4c07cd06fc93ac2da310e872f9483f92f888c72
Tadashi Abe <tadashi.abe@gmail.com>
2017-11-29 19:18:57 +0900

    Cosmetic fixes with pylint
    No functional change.

--- git-dump-commit.py
+++ git-dump-commit.py
@@ -45,14 +45,14 @@ import shutil
 import subprocess
 import sys
 
-logger = logging.getLogger(__name__)
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
+LOGGER = logging.getLogger(__name__)
+CH = logging.StreamHandler()
+CH.setLevel(logging.INFO)
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(CH)
 
-destdir = "DUMP-COMMIT"
-linux_kernel_repos = [
+DEST_DIR = "DUMP-COMMIT"
+LINUX_KERNEL_REPOS = [
     'git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux',
     'git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux-2.6',
     'kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux',
@@ -69,7 +69,7 @@ def _output_progress(count, total, name=None):
         total: an integer of the total number of commit.
         name: a str of the name of patch file.
     """
-    if logger.getEffectiveLevel() == logging.DEBUG:
+    if LOGGER.getEffectiveLevel() == logging.DEBUG:
         if name is not None:
             sys.stdout.write('{0}\n'.format(name))
     else:
@@ -105,11 +105,11 @@ class DumpGenerator(object):
                         re.compile(r'\.\.\.'), re.compile(r'\.*$|^-|-$'), re.compile(r'--*')]
         self.pc_name_max = os.pathconf('/tmp', 'PC_NAME_MAX')
         self.revision_range = []
-        if not os.path.exists(head_dir) or not os.path.exists(os.path.join(destdir, '.gitdump')) \
-                or not os.path.exists(os.path.join(destdir, '.gitdump', 'DUMP_HEAD')):
+        if not os.path.exists(head_dir) or not os.path.exists(os.path.join(DEST_DIR, '.gitdump')) \
+                or not os.path.exists(os.path.join(DEST_DIR, '.gitdump', 'DUMP_HEAD')):
             _init_meta_dir(head_dir)
 
-    def config(self, outdir, patchnum, revision_range=[]):
+    def config(self, outdir, patchnum, revision_range=None):
         """Changes digit and outdir. And resets offset."""
         if patchnum < 1000:
             patchnum = 1000
@@ -123,16 +123,16 @@ class DumpGenerator(object):
 
     def dump(self, commit_list):
         """Dumps each commit to file actually."""
-        commitID = ''
+        commit_id = ''
         pos = 0
         total = len(commit_list)
         # Run 'git show' and get the commit.
-        for commitID in commit_list:
+        for commit_id in commit_list:
             try:
-                patch = subprocess.check_output(['git', 'show', commitID], shell=False,
+                patch = subprocess.check_output(['git', 'show', commit_id], shell=False,
                                                 stderr=subprocess.STDOUT)
-            except subprocess.CalledProcessError as e:
-                logger.error('\n\n{0}'.format(e.output.decode('utf-8')))
+            except subprocess.CalledProcessError as error:
+                LOGGER.error('\n\n%s', error.output.decode('utf-8'))
                 sys.exit(1)
             # Extract subject
             name = patch.splitlines()[4].strip().decode('utf-8', 'ignore')
@@ -146,17 +146,17 @@ class DumpGenerator(object):
             name = template % (self.offset, name)
             if len(name) > self.pc_name_max:
                 name = name[:self.pc_name_max - 6] + ".patch"
-            with open(os.path.join(self.outdir, name), "wb") as f:
-                f.write(patch)
+            with open(os.path.join(self.outdir, name), "wb") as dump_file:
+                dump_file.write(patch)
             self.offset += 1
             pos += 1
             _output_progress(pos, total, name)
-        if commitID == '':
+        if commit_id == '':
             return
-        if self.revision_range == [] or self.revision_range[1] == 'HEAD':
-            with open(os.path.join(destdir, '.gitdump', 'DUMP_HEAD'),
+        if self.revision_range is None or self.revision_range[1] == 'HEAD':
+            with open(os.path.join(DEST_DIR, '.gitdump', 'DUMP_HEAD'),
                       'wb') as dump_head:
-                dump_head.write(b'%s\t%d\n' % (commitID, self.offset - 1))
+                dump_head.write(b'%s\t%d\n' % (commit_id, self.offset - 1))
         _output_progress(pos, total)
         sys.stdout.write('\n')
 
@@ -201,8 +201,8 @@ def _get_tag():
     try:
         out = subprocess.check_output(['git', 'tag'],
                                       shell=False, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        error = e.output
+    except subprocess.CalledProcessError as err:
+        error = err.output
         return (None, error)
     out = out.split()
     res = sorted(out, key=_key_linux_kernel)
@@ -226,8 +226,8 @@ def _get_commit_list(start='', end=''):
     try:
         commit_list = subprocess.check_output(cmd_and_args,
                                               shell=False, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        logger.error('\n\n{0}'.format(e.output.decode('utf-8')))
+    except subprocess.CalledProcessError as err:
+        LOGGER.error('\n\n%s', err.output.decode('utf-8'))
         sys.exit(1)
     commit_list = commit_list.splitlines()
     commit_list.reverse()
@@ -248,10 +248,10 @@ def _setup_dump_dir(tag):
     done = False
     rc_release = True
     if tag == 'HEAD':
-        return (done, rc_release, '{0}/{1}'.format(destdir, tag))
+        return (done, rc_release, '{0}/{1}'.format(DEST_DIR, tag))
     version = tag.split('-')[0]
-    outdir = '{0}/{1}/{2}'.format(destdir, version, tag)
-    if not os.path.exists('{0}/{1}'.format(destdir, version)):
+    outdir = '{0}/{1}/{2}'.format(DEST_DIR, version, tag)
+    if not os.path.exists('{0}/{1}'.format(DEST_DIR, version)):
         os.makedirs(outdir)
     elif not os.path.exists(outdir):
         os.mkdir(outdir)
@@ -266,9 +266,9 @@ def _init_meta_dir(head_dir):
     """Initializes HEAD directory and .gitdump where meta file is located at.
     """
     shutil.rmtree(head_dir, ignore_errors=True)
-    shutil.rmtree(os.path.join(destdir, '.gitdump'), ignore_errors=True)
+    shutil.rmtree(os.path.join(DEST_DIR, '.gitdump'), ignore_errors=True)
     os.mkdir(head_dir)
-    os.mkdir(os.path.join(destdir, '.gitdump'))
+    os.mkdir(os.path.join(DEST_DIR, '.gitdump'))
     return
 
 
@@ -286,16 +286,16 @@ def _fast_forward_commit_list(commit_list, head_dir):
     """
     # If head_dir or DUMP-COMMIT/.gitdump or DUMP-COMMIT/.gitdump/DUMP_HEAD doesn't exist,
     # it can't track current status. Do full dump.
-    if not os.path.exists(head_dir) or not os.path.exists(os.path.join(destdir, '.gitdump')) \
-       or not os.path.exists(os.path.join(destdir, '.gitdump', 'DUMP_HEAD')):
+    if not os.path.exists(head_dir) or not os.path.exists(os.path.join(DEST_DIR, '.gitdump')) \
+       or not os.path.exists(os.path.join(DEST_DIR, '.gitdump', 'DUMP_HEAD')):
         _init_meta_dir(head_dir)
         return (commit_list, None)
 
     try:
-        with open(os.path.join(destdir, '.gitdump', 'DUMP_HEAD'), 'rb') as f:
-            last_commit, offset = f.read().split()
+        with open(os.path.join(DEST_DIR, '.gitdump', 'DUMP_HEAD'), 'rb') as dump_head:
+            last_commit, offset = dump_head.read().split()
         offset = int(offset)
-    except:
+    except OSError:
         _init_meta_dir(head_dir)
         return (commit_list, None)
 
@@ -311,15 +311,15 @@ def _fast_forward_commit_list(commit_list, head_dir):
         _init_meta_dir(head_dir)
         return (commit_list, None)
 
-    with open(os.path.join(head_dir, patch[0]), 'rb') as f:
-        commitID = f.readline().strip().split()[1]
-    if commitID != last_commit:
+    with open(os.path.join(head_dir, patch[0]), 'rb') as last_commit_file:
+        commit_id = last_commit_file.readline().strip().split()[1]
+    if commit_id != last_commit:
         # Mismatch of last commit ID in DUMP-COMMIT/.gitdump/DUMP_HEAD.
         return (commit_list, None)
 
     try:
         index = commit_list.index(last_commit)
-    except:
+    except ValueError:
         return (commit_list, None)
     if offset == len(commit_list):
         # No new commit exists.
@@ -335,12 +335,12 @@ def _check_linux_kernel():
     and dumps all the commits of each tag.
     """
     (revs, error) = _get_tag()
-    if (error):
-        logger.error('\n\n{0}'.format(error.decode('utf-8')))
+    if error:
+        LOGGER.error('\n\n%s', error.decode('utf-8'))
         sys.exit(1)
 
     end = ''
-    dump_generator = DumpGenerator(os.path.join(destdir, 'HEAD'))
+    dump_generator = DumpGenerator(os.path.join(DEST_DIR, 'HEAD'))
     for revision in revs:
         start = end
         end = revision
@@ -349,63 +349,59 @@ def _check_linux_kernel():
         (done, rc_release, outdir) = _setup_dump_dir(end)
         if done:
             if not rc_release:
-                logger.info("Skipping {0:12s} (already done)".format(end))
+                LOGGER.info("Skipping {0:12s} (already done)".format(end))
             continue
         commit_list = _get_commit_list(start, end)
         if len(commit_list) == 1 and commit_list[0] == '':
             # empty version
-            logger.info("Skipping {0:12s} (empty)".format(end))
+            LOGGER.info("Skipping {0:12s} (empty)".format(end))
             continue
         dump_generator.config(outdir, len(commit_list), [start, end])
         (commit_list, offset) = _fast_forward_commit_list(commit_list,
-                                                          os.path.join(destdir, 'HEAD'))
+                                                          os.path.join(DEST_DIR, 'HEAD'))
         if offset:
             dump_generator.update_offset(offset)
         if commit_list != []:
-            logger.info("Processing {0}..{1}".format(start, end))
+            LOGGER.info("Processing %s..%s", start, end)
             dump_generator.dump(commit_list)
         else:
-            logger.info("Processing {0:10s} (up to date)".format(end))
+            LOGGER.info("Processing {0:10s} (up to date)".format(end))
 
 
 def _check_git_repo():
     """Dump all the commits of current branch.
     """
-    dump_generator = DumpGenerator(destdir)
+    dump_generator = DumpGenerator(DEST_DIR)
     commit_list = _get_commit_list()
-    dump_generator.config(destdir, len(commit_list))
-    (commit_list, offset) = _fast_forward_commit_list(commit_list, destdir)
+    dump_generator.config(DEST_DIR, len(commit_list))
+    (commit_list, offset) = _fast_forward_commit_list(commit_list, DEST_DIR)
     if offset:
         dump_generator.update_offset(offset)
     if commit_list != []:
         dump_generator.dump(commit_list)
 
 
-def main():
+if __name__ == "__main__":
     # Check if you're in git repo.
     try:
         repo = subprocess.check_output(['git', 'config', 'remote.origin.url'],
                                        shell=False, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        logger.error('\n\n{0}'.format(e.output.decode('utf-8')))
+    except subprocess.CalledProcessError as err:
+        LOGGER.error('\n\n%s', err.output.decode('utf-8'))
         sys.exit(1)
 
     if '-v' in sys.argv:
         # verbose output
-        handler.setLevel(logging.DEBUG)
-        logger.setLevel(logging.DEBUG)
+        CH.setLevel(logging.DEBUG)
+        LOGGER.setLevel(logging.DEBUG)
 
-    logger.info("Destination directory: {0}".format(destdir))
-    if not os.path.exists(destdir):
-        os.mkdir(destdir)
+    LOGGER.info("Destination directory: %s", DEST_DIR)
+    if not os.path.exists(DEST_DIR):
+        os.mkdir(DEST_DIR)
 
     repo = re.sub(r'''^(git|https)://''', '', repo.decode('utf-8'))
     repo = repo.rstrip('.git\n')
-    if repo in linux_kernel_repos:
+    if repo in LINUX_KERNEL_REPOS:
         _check_linux_kernel()
     else:
         _check_git_repo()
-
-
-if __name__ == "__main__":
-    main()
